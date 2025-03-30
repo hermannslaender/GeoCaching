3ff06d501b1840c89bf9406b36eda32cdb8d9b41
spelter1967 <andreas@spelter.net>
2025-03-30 11:56:28 +0200

    Update GeoCaching.py

--- GeoCaching.py
+++ GeoCaching.py
@@ -2,60 +2,65 @@ import os
 import shutil
 import re
 import zipfile
+import time
+
 
 source_directory = "D:/Downloads/"
 target_directory = "D:/OneDrive/Dokumente/GeoCaching/gpx/"
+
 
 def gpx_cleaner(filename, HTML = None):
     with open(filename, 'r', encoding='utf-8') as file:
         gpx_content = file.read()
 
     #print(f"- bereinige {filename}")
-    gpx_content = re.sub(r"<gpx.*?><wpt lat", "<gpx><wpt lat", gpx_content, flags=re.DOTALL)
-    gpx_content = re.sub(r'<wpt lat="0" lon="0">.*?</wpt>', "", gpx_content, flags=re.DOTALL)
-    gpx_content = re.sub(r"<time>.*?</time>", "", gpx_content, flags=re.DOTALL)
+    gpx_content = re.sub(r"<gpx.*?><wpt lat", "<gpx><wpt lat", gpx_content, flags=re.DOTALL) # entfernt den überflüsisgen Header
+    gpx_content = re.sub(r'<wpt lat="0" lon="0">.*?</wpt>', "", gpx_content, flags=re.DOTALL) # entfernt Wegpunkte ohne Koordinaten
+    gpx_content = re.sub(r"<time>.*?</time>", "", gpx_content, flags=re.DOTALL) # entfernt die Uhrzeit
     #gpx_content = re.sub(r"<url>.*?</url>", "", gpx_content, flags=re.DOTALL)
-    #gpx_content = re.sub(r"<urlname>.*?</urlname>", "", gpx_content, flags=re.DOTALL)
-    if HTML:
+    #gpx_content = re.sub(r"<urlname>.*?</urlname>", "", gpx_content, flags=re.DOTALL) # wird als Name im Garmin benötigt
+    if HTML: # logs sind in Listen und PocketQueries unterschiedlich, entfernt die Kurzbeschreibung
         gpx_content = re.sub(r"<groundspeak:short_description html.*?</groundspeak:short_description>", "", gpx_content, flags=re.DOTALL)
     else:
         gpx_content = re.sub(r"<groundspeak:short_description>.*?</groundspeak:short_description>", "", gpx_content, flags=re.DOTALL)
-    if HTML:
+    if HTML: # logs sind in Listen und PocketQueries unterschiedlich, entfernt die Langbeschreibung
         gpx_content = re.sub(r"<groundspeak:long_description html.*?</groundspeak:long_description>", "", gpx_content, flags=re.DOTALL)
     else:
         gpx_content = re.sub(r"<groundspeak:long_description>.*?</groundspeak:long_description>", "", gpx_content, flags=re.DOTALL)
-    gpx_content = re.sub(r"<groundspeak:encoded_hints>.*?</groundspeak:encoded_hints>", "", gpx_content, flags=re.DOTALL)
-    gpx_content = re.sub(r"<groundspeak:logs>.*?</groundspeak:logs>", "", gpx_content, flags=re.DOTALL)
-    gpx_content = re.sub(r"<wpt[^>]*>(?:(?!<groundspeak:cache).)*?</wpt>", "", gpx_content, flags=re.DOTALL)
-    gpx_content = re.sub(r"^\s*$\n", "", gpx_content, flags=re.MULTILINE)
+    gpx_content = re.sub(r"<groundspeak:encoded_hints>.*?</groundspeak:encoded_hints>", "", gpx_content, flags=re.DOTALL) # entfernt Hints
+    gpx_content = re.sub(r"<groundspeak:logs>.*?</groundspeak:logs>", "", gpx_content, flags=re.DOTALL) # entfernt Logs
+    gpx_content = re.sub(r"<wpt[^>]*>(?:(?!<groundspeak:cache).)*?</wpt>", "", gpx_content, flags=re.DOTALL) # entfernt Wegpunkite ohne die Cache-Information
+    gpx_content = re.sub(r"^\s*$\n", "", gpx_content, flags=re.MULTILINE) # entfernt Leerzeilen
 
     print("-", gpx_content.count("<wpt lat"), "GeoCaches")
     #print(gpx_content)
 
-    #print(f"- lösche {filename}") # lösche unbereinigte datei
-    os.remove(filename)
+    #print(f"- lösche {filename}")
+    os.remove(filename) # löscht unbereinigte datei
 
     filename = filename.replace(".gpx", "_clean.gpx")
-    #print(f"- erzeuge {filename}") # erzeuge bereinigte datei
-    with open(filename, 'w', encoding='utf-8') as file:
+    #print(f"- erzeuge {filename}")
+    with open(filename, 'w', encoding='utf-8') as file: # erzeuge bereinigte datei
         file.write(gpx_content)
 
     return filename
 
 
 def unzip_pocket_query(filename):
-    #print(f"- entpacke {filename}") # auspacken
+    #print(f"- entpacke {filename}")
     global source_directory
-    with zipfile.ZipFile(filename, 'r') as zip_ref:
+    with zipfile.ZipFile(filename, 'r') as zip_ref: # auspacken
          zip_ref.extractall(source_directory)
-    #print(f"- lösche {filename}") # lösche ZIP
-    os.remove(filename)
+    #print(f"- lösche {filename}")
+    os.remove(filename) # löscht ZIP
     filename = filename.replace(".zip", "-wpts.gpx")
-    #print(f"- lösche {filename}") # lösche WPTS
-    os.remove(filename)
+    #print(f"- lösche {filename}")
+    os.remove(filename) # löscht WPTS
     filename = filename.replace("-wpts.gpx", ".gpx")
     return filename
 
+if len(os.listdir(source_directory)) == 0:
+    print("keine Datei gefunden")
 
 for filename in os.listdir(source_directory):
     match filename:
@@ -63,8 +68,8 @@ for filename in os.listdir(source_directory):
             print("GeoCaching-Liste: ", filename)
             clean_file = gpx_cleaner(source_directory+filename, HTML = False)
             file_name = target_directory+filename
-            #print(f"- verschiebe {filename}")  # verschiebe bereinigte GPX
-            shutil.move(clean_file, file_name)
+            #print(f"- verschiebe {filename}")
+            shutil.move(clean_file, file_name) # verschiebt bereinigte GPX
 
         case _ if re.match(r"^\d{8}_.+\.zip$", filename):
             print("PocketQuery: ", source_directory+filename)
@@ -77,3 +82,4 @@ for filename in os.listdir(source_directory):
         case _:
             print("Sonstige: ", filename)
 
+time.sleep(3)
